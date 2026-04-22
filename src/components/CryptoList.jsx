65552bf115f87a0d87f2e36f18bbb0ceff41c944
Jomy-Joy <jomyjoy200@gmail.com>
2026-04-22 22:20:15 +0530

    [api fetch for crypto list & loading]

--- src/components/CryptoList.jsx
+++ src/components/CryptoList.jsx
@@ -1,23 +1,42 @@
 import React from "react";
-
+import axios from "axios";
 const CryptoList = () => {
-  const crypto = [
-    { name: "Bitcoin", price: "$67,000" },
-    { name: "Ethereum", price: "$3,200" },
-    { name: "Solana", price: "$120" }
-  ];
-
+  const [crypto, setCrypto] = React.useState([]);
+  const fetchCrypto = async () => {
+    try {
+      const response = await axios.get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd",
+      );
+      setCrypto(response?.data || []);
+    } catch (e) {
+      console.error("Error fetching crypto data", e);
+    }
+  };
+  React.useEffect(() => {
+    fetchCrypto();
+  }, []);
+  if (!crypto.length) return <div>Loading...</div>;
   return (
     <div>
-      {crypto.map((item, index) => (
-        <div key={index} style={{
-          display: "flex",
-          justifyContent: "space-between",
-          padding: "10px",
-          borderBottom: "1px solid #eee"
-        }}>
-          <span>{item.name}</span>
-          <span>{item.price}</span>
+      {crypto.slice(0, 10).map((item, index) => (
+        <div
+          key={index}
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            padding: "10px",
+            borderBottom: "1px solid #eee",
+          }}
+        >
+          <div style={{ marginRight: "6px" }}>
+            <img
+              src={item.image}
+              alt="sysmbol"
+              style={{ width: "20px", height: "20px" }}
+            />
+          </div>
+          <span style={{ marginRight: "auto" }}>{item.name}</span>
+          <span>{item.current_price}</span>
         </div>
       ))}
     </div>
